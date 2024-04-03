30b2fa5c4bfb466e10370737c2eb172aafcdd449
Manoj <manojkumarbonala3@gmail.com>
2024-04-03 21:28:04 +0530

    Homepage added

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import AppBar from "../components/AppBar"
+
+export const Home = () => {
+    return(
+        <>
+            <div>
+                <AppBar />
+                <div className="flex justify-center items-center">
+                    Home page
+                </div>
+            </div>
+        </>
+    )
+}
